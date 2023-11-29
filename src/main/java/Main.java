24b2bc218a5d6c3e9ea7526f8f8fea2474e23676
Mitchell <13673076+mishyy@users.noreply.github.com>
2023-11-29 17:54:19 +1000

    Echo received packets back to requester

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,26 +2,24 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
-public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+public final class Main {
 
-    // Uncomment this block to pass the first stage
-    //
-    // try(DatagramSocket serverSocket = new DatagramSocket(2053)) {
-    //   while(true) {
-    //     final byte[] buf = new byte[512];
-    //     final DatagramPacket packet = new DatagramPacket(buf, buf.length);
-    //     serverSocket.receive(packet);
-    //     System.out.println("Received data");
-    //
-    //     final byte[] bufResponse = new byte[512];
-    //     final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
-    //     serverSocket.send(packetResponse);
-    //   }
-    // } catch (IOException e) {
-    //     System.out.println("IOException: " + e.getMessage());
-    // }
-  }
+    public static void main(final String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
+
+        try (final var socket = new DatagramSocket(2053)) {
+            final var buf = new byte[512];
+            final var packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+            System.out.println("Received data");
+
+            final var responseBuf = new byte[512];
+            final var responsePacket = new DatagramPacket(responseBuf, responseBuf.length, packet.getSocketAddress());
+            socket.send(responsePacket);
+        } catch (final IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
 }
